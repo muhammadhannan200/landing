fb68c31f3b2ecd1e4309d042eb1cd4fde7ff77a8
muhammadhannan200 <88384362+muhammadhannan200@users.noreply.github.com>
2022-10-27 20:17:10 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+let img=document.querySelector('.img');
+let container=document.querySelector('.container')
+/* img > cgange logo  
+.img change pic 
+*/
+function phones(phone){
+    img.src=phone;
+}
+function colors(colors){
+    container.style.background=colors;
+    
+
+}
